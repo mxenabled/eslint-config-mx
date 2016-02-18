6912c1d3d17d16a41a6a730593cd121d3c761518
Brandon Beeks <brandon@beeks.me>
2016-02-18 14:17:01 -0700

    Add Raja config changes

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ module.exports = {
     "no-self-compare": 2,
     "no-sequences": 2,
     "no-throw-literal": 2,
-    "no-unused-expressions": 0,
+    "no-unused-expressions": 2,
     "no-void": 2,
     "no-warning-comments": 0,
     "no-with": 2,
